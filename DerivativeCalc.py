742d984b57c1325a87ec389184da2b34a0351212
BigPeshh <48938077+BigPeshh@users.noreply.github.com>
2019-03-31 12:51:35 -0500

    Updated DerivativeCalc.py
    I was getting a notification that there was unused functions from the way I imported sympy, so I changed the formatting to get rid of it.

--- DerivativeCalc.py
+++ DerivativeCalc.py
@@ -1,9 +1,9 @@
-from sympy import *
+import sympy as sp
 
-x = Symbol('x')
+x = sp.Symbol('x')
 
 a = input("Equation: ")
 
-print("First Derivative: ", diff(a, x, 1))
-print("Second Derivative: ", diff(a, x, 2))
-print("Thrid Derivative: ", diff(a, x, 3))
+print("First Derivative: ", sp.diff(a, x, 1))
+print("Second Derivative: ", sp.diff(a, x, 2))
+print("Thrid Derivative: ", sp.diff(a, x, 3))
