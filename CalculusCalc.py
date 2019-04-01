66e7b9f4358aaff22c2a806fe49efde094bf700c
BigPeshh <48938077+BigPeshh@users.noreply.github.com>
2019-04-01 14:57:30 -0500

    Updated CalculusCalc.py
    Took out LimitCalc import

--- CalculusCalc.py
+++ CalculusCalc.py
@@ -6,7 +6,6 @@ print("____________________________________________________")
 print(" ")
 print("                   1. Derivative                    ")
 print("                  2. Integration                    ")
-print("                     3. Limit                       ")
 print("____________________________________________________")
 
 choice = int(input("Operation Number: "))
@@ -15,5 +14,3 @@ if choice == '1':
     import DerivativeCalc
 elif choice == '2':
     import IntegralCalc
-elif choice == '3':
-    import LimitCalc
