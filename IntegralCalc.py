d313e1caea9b65a0a3441be7208d00734c58f38b
BigPeshh <48938077+BigPeshh@users.noreply.github.com>
2019-04-01 14:50:33 -0500

    Updated IntegralCalc.py
    Here is the code for the integral calculator.

--- IntegralCalc.py
+++ IntegralCalc.py
@@ -1,1 +1,15 @@
-print("Fill in code")
+import sympy as sp 
+
+print("====================================================")
+print("               Integral Calculator                  ")
+print("====================================================")
+print(" ")
+
+x = sp.Symbol('x')
+f = input("Equation: ")
+g = sp.integrate(f, x)
+h = sp.integrate(g, x)
+
+print("First Integration: ", sp.integrate(f, x))
+print("Second Integration: ", sp.integrate(g, x))
+print("Third Integration: ", sp.integrate(h, x))
